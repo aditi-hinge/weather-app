ff881ad9e7f5906391ac8ebd24d6e545a6ef8234
aditi-hinge <aditi.hinge@gmail.com>
2022-04-10 14:40:14 +0900

    conversion for neg nums fixed

--- src/modules/conversion.js
+++ src/modules/conversion.js
@@ -6,22 +6,24 @@ function conversion(){
     const conversionButton = document.getElementById('conversion-button');
     const elementsOfTemp = document.querySelectorAll('.temp');
 
+    // convert from celcius to fahrenheit
     if (conversionButton.textContent === 'C°') {
         elementsOfTemp.forEach((element) => {
             const text = element.textContent;
-            const filteredValue = text.match(/\d+\.\d+/).toString().replace(/,/g, '');
-            const fahrenheitValue = ((filteredValue * (9/5)) + 32).toFixed(1);
+            const filteredValue = text.match(/-?\d+\.\d+/).toString().replace(/,/g, '');
+            const fahrenheitValue = ((1.8 * filteredValue) + 32).toFixed(1);
             element.textContent = `${fahrenheitValue}°`;
         })
 
         conversionButton.textContent = 'F°';
         return;
     }
+    // convert from fahrenheit to celcius
     if (conversionButton.textContent === 'F°') {
         elementsOfTemp.forEach((element) => {
             const text = element.textContent;
-            const filteredValue = text.match(/\d+\.\d+/).toString().replace(/,/g, '');
-            const celciusValue = ((filteredValue - 32) * (5/9)).toFixed(1);
+            const filteredValue = text.match(/-?\d+\.\d+/).toString().replace(/,/g, '');
+            const celciusValue = ((5/9) * (filteredValue - 32)).toFixed(1);
             element.textContent = `${celciusValue}°`;
         })
 
